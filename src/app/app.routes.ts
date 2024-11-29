e8cf8f4f793c77af31ce94d2aad45909c01aa07b
Dharati-Kamble <dkamble031993@gmail.com>
2024-11-29 23:17:10 +0530

    Initial commit for Route App Third

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { BooksComponent } from './books/books.component';
+import { WrongChoiceComponent } from './wrong-choice/wrong-choice.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'technologies', component: TechnologiesComponent },
+  { path: 'books', component: BooksComponent },
+  { path: '', redirectTo: '/technologies', pathMatch: 'full' },  // Default route
+  { path: '**', component: WrongChoiceComponent }  // Catch-all for invalid paths
+];
